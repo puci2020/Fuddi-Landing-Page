2cb5ad0acc5586acd7e76879780ddecec1dc143b
puci2020 <adam.pucicki97@gmail.com>
2020-08-18 18:30:24 +0200

    Updates

---
+++ precache-manifest.950b2960d696c7435ae11d874835da66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a876ad7de15d0ff1fcffc3f5acab16c6",
+    "url": "/Fuddi-Landing-Page/index.html"
+  },
+  {
+    "revision": "1823cc59ace1c4f36dde",
+    "url": "/Fuddi-Landing-Page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e84896347e3cce22bb0",
+    "url": "/Fuddi-Landing-Page/static/js/2.23bef678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fuddi-Landing-Page/static/js/2.23bef678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1823cc59ace1c4f36dde",
+    "url": "/Fuddi-Landing-Page/static/js/main.d5f0b15a.chunk.js"
+  },
+  {
+    "revision": "a035f5d3695709884022",
+    "url": "/Fuddi-Landing-Page/static/js/runtime-main.dd35f3a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Fuddi-Landing-Page/static/media/logo.5d5d9eef.svg"
+  }
+]);
