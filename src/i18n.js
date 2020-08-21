b526798d92c07c55eb7f21f43c3cd0f102bf8652
puci2020 <adam.pucicki97@gmail.com>
2020-08-21 13:29:07 +0200

    fix translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,54 +1,41 @@
-import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-const fallbackLng = ['en'];
-const availableLanguages = ['en', 'pl'];
+import Backend from "i18next-xhr-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-
-const options = {
-  // order and from where user language should be detected
-  order: [ 'navigator', 'htmlTag', 'path', 'subdomain'],
-
-  // keys or params to lookup language from
-  lookupQuerystring: 'lng',
-  lookupCookie: 'i18next',
-  lookupLocalStorage: 'i18nextLng',
-  lookupFromPathIndex: 0,
-  lookupFromSubdomainIndex: 0,
-
-  // cache user language on
-  caches: ['localStorage', 'cookie'],
-  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
-
-  // optional expire and domain for set cookie
-  cookieMinutes: 10,
-  cookieDomain: 'myDomain',
-
-  // optional htmlTag with lang attribute, the default is:
-  htmlTag: document.documentElement,
-
-  // only detect languages that are in the whitelist
-  checkWhitelist: true
-}
+import translationEN from "./locales/en/translation.json";
+import translationPL from "./locales/pl/translation.json";
+const languages = ["en", "pl"];
+const resources = {
+    en: {
+        translation: translationEN,
+    },
+    pl: {
+        translation: translationPL,
+    },
+};
 
 i18n
-  .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
-
-  .use(LanguageDetector) // detect user language
-
-  .use(initReactI18next) // pass the i18n instance to react-i18next.
-
-  .init({
-    fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
-    debug: true,
-    whitelist: availableLanguages,
-    detection: options,
-
-    interpolation: {
-      escapeValue: false
-    },
-  });
+// load translation using xhr -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+// learn more: https://github.com/i18next/i18next-xhr-backend
+    .use(Backend)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        fallbackLng: "pl",
+        debug: true,
+        lng: "pl",
+        resources,
+        whitelist: languages,
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+    });
 
 export default i18n;
