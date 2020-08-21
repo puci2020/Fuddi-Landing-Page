6e50d2bc707f18856aea21ddd64f7bc62e6628c3
puci2020 <adam.pucicki97@gmail.com>
2020-08-21 12:56:04 +0200

    add and fix translation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-// import Navbar from "../Navbar/Navbar";
 import "./Home.css"
+import {useTranslation} from "react-i18next";
 
 function Home() {
+    const {t} = useTranslation();
+
+
     return (
         <div className="home">
             {/*<Navbar/>*/}
+            <p>{t("link1.1")}</p>
         </div>
     );
 }
--- src/Components/Navlist/Navlist.js
+++ src/Components/Navlist/Navlist.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import "./Navlist.css"
+import {useTranslation} from "react-i18next";
+
+
 
 function NavList()
 {
+    const {t, i18n} = useTranslation();
+
+    function changeLanguage() {
+        const lg = document.getElementById('lang').value;
+        i18n.changeLanguage(lg);
+
+    }
+      function changeLanguageBtn(lang){
+        i18n.changeLanguage(lang);
+    }
+
     return (
         <div className="nav">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Droga do sukcesu</a></li>
-                <li><a href="#">O Nas</a></li>
-                <li><a href="#">FAQ</a></li>
+                <li><a href="#">{t("link1.1")}</a></li>
+                <li><a href="#">{t('link2.1')}</a></li>
+                <li><a href="#">{t('link3.1')}</a></li>
+                <li><a href="#">{t('link4.1')}</a></li>
+                <li><select id="lang" onChange={()=>changeLanguage()}>
+                    <option value={'pl'}>Polski</option>
+                    <option value={'en'}>English</option>
+                </select></li>
+                <ul id="lang2">
+                    <button onClick={()=>changeLanguageBtn('pl')}>Polski</button>
+                    <button onClick={()=>changeLanguageBtn('en')}>English</button>
+                </ul>
             </ul>
+
+
         </div>
     );
 
