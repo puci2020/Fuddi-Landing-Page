3c48e847a786e59dd96bb5e134d2633929b8a917
puci2020 <adam.pucicki97@gmail.com>
2020-08-24 10:58:41 +0200

    Updates

---
+++ precache-manifest.15d4b608c22955e1bc7440f446e57e17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3805318d934b7838848e494f12e8e1",
+    "url": "/Fuddi-Landing-Page/index.html"
+  },
+  {
+    "revision": "75e8142bc5b89700860f",
+    "url": "/Fuddi-Landing-Page/static/css/main.efe1df0b.chunk.css"
+  },
+  {
+    "revision": "5903ac5740dd7e65229e",
+    "url": "/Fuddi-Landing-Page/static/js/2.1f5fd474.chunk.js"
+  },
+  {
+    "revision": "75e8142bc5b89700860f",
+    "url": "/Fuddi-Landing-Page/static/js/main.809ffcfb.chunk.js"
+  },
+  {
+    "revision": "ffe53f373f165d290964",
+    "url": "/Fuddi-Landing-Page/static/js/runtime~main.e342184c.js"
+  },
+  {
+    "revision": "e0aab94f4498ab5c22d167deb6c0ff75",
+    "url": "/Fuddi-Landing-Page/static/media/app.e0aab94f.png"
+  },
+  {
+    "revision": "b3c26dab66b5d989e4738ec698d3c3a5",
+    "url": "/Fuddi-Landing-Page/static/media/field3.b3c26dab.jpeg"
+  },
+  {
+    "revision": "8329ff528cafb3bee875d627f01eabb3",
+    "url": "/Fuddi-Landing-Page/static/media/logo.8329ff52.png"
+  },
+  {
+    "revision": "06bf648d094166952dcd30c255fc3fc2",
+    "url": "/Fuddi-Landing-Page/static/media/white3.06bf648d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fuddi-Landing-Page/precache-manifest.36de6605cbd00cf84cb30d8bfc266a7e.js"
+  "/Fuddi-Landing-Page/precache-manifest.15d4b608c22955e1bc7440f446e57e17.js"
 );
 
 self.addEventListener('message', (event) => {
