e6cc8fdb32663b905c3d7349db052bcb5df643f7
puci2020 <adam.pucicki97@gmail.com>
2020-08-27 16:26:36 +0200

    Add boxes in about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import "./About.css"
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import shoppingcart from '../../img/icons/shoppingcart.svg'
+import vegetable from '../../img/icons/vegetable.svg'
+import user from '../../img/icons/user.png'
+import farmer from '../../img/icons/farmer.png'
+import delivery from '../../img/icons/delivery.png'
 
 
 function About() {
@@ -46,34 +49,64 @@ function About() {
                 <h2>Jakie wartości chcemy, aby niosła aplikacja</h2>
                 <div className="row">
 
-                    <div className="one">
+                    <div className="item one">
                         <div className="box first">
-                            <div className="icon lg">asdasd</div>
-                            <div className="text">asdkljasldkj</div>
+                            <div className="icon">
+                                <img src={shoppingcart} alt="Shopping cart"/>
+                            </div>
+                            <div className="text">Tworzymy platformę e-commerce
+                                dzięki, której codzienne zakupy będą proste i szybkie.
+                            </div>
                         </div>
                     </div>
-                    <div className="two">
+                    <div className="item two">
                         <div className="box first">
-                            ggg
+                            <div className="icon">
+                                <img src={vegetable} alt="Shopping cart"/>
+                            </div>
+                            <div className="text">Oferowane produkty będą najwyższej jakości w przystępnej cenie. Zależy
+                                nam na pokazaniu że wyroby wytyarzane przy użyciu rąk są nie tlko samczaniejsze ale mają
+                                w sobie cząśtę swojego twórcy lub w przypdaku owoców i warzyw akcent osoby która o nich
+                                dbała.
+                            </div>
                         </div>
                     </div>
 
                 </div>
                 <h2>Pragniemy połączyć trzy grupy użytkowników Fuddi</h2>
                 <div className="row">
-                    <div className="five">
+                    <div className="item five">
                         <div className="box sec">
-                            asd
+                            <div className="icon sm">
+                                <img src={user} alt="Shopping cart"/>
+                            </div>
+                            <div className="text small">
+                                <h4>Klienci</h4>
+                                <p>Jeśli nie byłoby klienta nie byłoby aplikacji.</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="six">
+                    <div className="item six">
                         <div className="box sec">
-                            asd
+                            <div className="icon sm">
+                                <img src={farmer} alt="Shopping cart"/>
+                            </div>
+                            <div className="text small">
+                                <h4>Producenci</h4>
+                                <p>Rolnicy jak i rzemieślnicy, to oni dzięki sowjej cięzkiej pracy wytwarzają produkt najwyższej jakości.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="seven">
+                    <div className="item seven">
                         <div className="box sec">
-                            asd
+                            <div className="icon sm">
+                                <img src={delivery} alt="Shopping cart"/>
+                            </div>
+                            <div className="text small">
+                                <h4>Dostawcy</h4>
+                                <p>Każda osoba, która przy okazji podróży zadklaruje chęć dostarczenia towaru od Producenta do Klienta. </p>
+                            </div>
                         </div>
                     </div>
                 </div>
