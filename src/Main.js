ae511a61b3e29c0cc84ed03ce7299286ad647a74
puci2020 <adam.pucicki97@gmail.com>
2020-09-01 23:36:29 +0200

    fix react router

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,76 @@
-
-
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-import App from './App'
-import Faq from "./Faq";
-// import About from './Components/About/About'
-
+import React, {Component} from 'react';
+import './App.css';
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Navbar from "./Components/Navbar/Navbar";
+import SideDrawer from "./Components/SideDrawer/SideDrawer";
+import {ParallaxProvider} from 'react-scroll-parallax';
+import Roadmap from "./Components/Roadmap/Roadmap";
+import Footer from "./Components/Footer/Footer";
+import Screens from "./Components/Screens/Screens";
+import Team from "./Components/Team/Team";
 
 class Main extends Component {
+    state = {
+        sideDrawerOpen: false,
+        active: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return {
+                sideDrawerOpen: !prevState.sideDrawerOpen,
+            };
+        });
+        this.setState((prevState) => {
+            return {
+                active: !prevState.active
+            }
+        });
+        if (!this.state.active) {
+            // console.log("działą");
+            document.getElementById('secound').style.display = "none";
+            document.getElementById('first').style.transform = "translateY(8px) rotate(45deg)";
+            document.getElementById('third').style.transform = "translateY(-8px) rotate(-45deg)";
+        } else {
+            // console.log("nie")
+            document.getElementById('secound').style.display = "block";
+            document.getElementById('first').style.transform = "rotate(0)";
+            document.getElementById('first').style.marginTop = "0";
+            document.getElementById('third').style.transform = "rotate(0)";
+            document.getElementById('third').style.marginBottom = "0";
+        }
+    };
+
     render() {
+        let sideDrawer = <SideDrawer/>;
+
+        if (this.state.sideDrawerOpen) {
+            sideDrawer = <SideDrawer show={this.state.sideDrawerOpen}/>
+        }
+
+        // let navBar = <Navbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        //
+        // if (this.state.active){
+        //     navBar = <Navbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        // }
         return (
-            <Router>
-                <Route path={"/"} exact component={App}/>
-                {/*<Route path={"/about"} component={About} render={()=><Redirect to="/"/>}/>*/}
-                <Route path={"/faq"} component={Faq}/>
-            </Router>
+            <div className="App">
+                <ParallaxProvider>
+                    <Home/>
+                </ParallaxProvider>
+                <Navbar drawerClickHandler={this.drawerToggleClickHandler}/>
+                {/*{navBar}*/}
+                {sideDrawer}
+                <ParallaxProvider>
+                    <Screens/>
+                </ParallaxProvider>
+                <About/>
+                <Team/>
+                <Roadmap/>
+                <Footer/>
+
+            </div>
         );
     }
 }
