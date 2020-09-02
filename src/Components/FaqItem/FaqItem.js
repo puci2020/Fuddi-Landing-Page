a6aff78225855122c8fbef3d91dc63c67b703c1d
puci2020 <adam.pucicki97@gmail.com>
2020-09-02 14:01:50 +0200

    Add complete faq items nad translations

--- src/Components/FaqItem/FaqItem.js
+++ src/Components/FaqItem/FaqItem.js
@@ -7,14 +7,19 @@ const Content = styled.div`
   width: 100%;
   height: 100%;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(2, auto);
+  grid-template-rows: repeat(4, auto);
   grid-gap: 60px;
   padding: 0 10%;
+  
+  @media (max-width: 1024px){
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(9, auto);
+  }
 `;
 
 const ContentItem = styled.div`
-  height: 400px;
+  height: auto;
   border-radius: 20px;
   background-color: rgba(43,57,71,85%);
   padding: 40px;
@@ -39,6 +44,38 @@ const FaqItem = () => {
                 <ContentItem>
                     <h4>{t('faq.quest1.header')}</h4>
                     <p>{t('faq.quest1.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest2.header')}</h4>
+                    <p>{t('faq.quest2.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest3.header')}</h4>
+                    <p>{t('faq.quest3.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest4.header')}</h4>
+                    <p>{t('faq.quest4.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest5.header')}</h4>
+                    <p>{t('faq.quest5.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest6.header')}</h4>
+                    <p>{t('faq.quest6.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest7.header')}</h4>
+                    <p>{t('faq.quest7.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest8.header')}</h4>
+                    <p>{t('faq.quest8.content')}</p>
+                </ContentItem>
+                <ContentItem>
+                    <h4>{t('faq.quest9.header')}</h4>
+                    <p>{t('faq.quest9.content')}</p>
                 </ContentItem>
             </Content>
         </div>
