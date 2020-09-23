eb26e4497254b4e3943e59d28941beb952b66b5a
puci2020 <adam.pucicki97@gmail.com>
2020-09-23 11:48:35 +0200

    Updates

---
+++ precache-manifest.c56e9c6cb833b3bb70495060ccb73886.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fec5bb0fae1b3e2513b6c79aadd753b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38023e0e2adbc06f4335",
+    "url": "/static/css/main.1dd88ba7.chunk.css"
+  },
+  {
+    "revision": "2f43bf1ff301be57f0e6",
+    "url": "/static/js/2.3acdb467.chunk.js"
+  },
+  {
+    "revision": "38023e0e2adbc06f4335",
+    "url": "/static/js/main.cecd9cad.chunk.js"
+  },
+  {
+    "revision": "2b5b85fd6e7177ede9b1",
+    "url": "/static/js/runtime~main.47f13b47.js"
+  },
+  {
+    "revision": "ba5c1a480e39a2cea18a2e35622ea232",
+    "url": "/static/media/1.ba5c1a48.jpg"
+  },
+  {
+    "revision": "17d7183a7cb8f5a6b14bb6afed306e9a",
+    "url": "/static/media/2.17d7183a.jpg"
+  },
+  {
+    "revision": "46c2c34675721eb1a7e834ad4a1d0337",
+    "url": "/static/media/3.46c2c346.jpg"
+  },
+  {
+    "revision": "8ba7516ee21d9b69c40cb4ef0ea02c0b",
+    "url": "/static/media/5.8ba7516e.jpg"
+  },
+  {
+    "revision": "2fb0f1f7e35735786bd04ebd4da48beb",
+    "url": "/static/media/6.2fb0f1f7.jpg"
+  },
+  {
+    "revision": "4b9a23c9c748c71e9add31be61cecc6d",
+    "url": "/static/media/aboutBG.4b9a23c9.jpeg"
+  },
+  {
+    "revision": "03c7fc3c8c2255ab3dff936bc08ddd12",
+    "url": "/static/media/adam_pucicki.03c7fc3c.jpeg"
+  },
+  {
+    "revision": "e0aab94f4498ab5c22d167deb6c0ff75",
+    "url": "/static/media/app.e0aab94f.png"
+  },
+  {
+    "revision": "6b9dd4f69791a3b7d1e13b689949955f",
+    "url": "/static/media/arrow.6b9dd4f6.svg"
+  },
+  {
+    "revision": "4c9b32d7ade241c884fa2d39b369fe4e",
+    "url": "/static/media/contact.4c9b32d7.png"
+  },
+  {
+    "revision": "f42a2bfebad2e99241fa6e89831e9620",
+    "url": "/static/media/delivery.f42a2bfe.svg"
+  },
+  {
+    "revision": "1e9f01162859ce7bec44274a13bb9d24",
+    "url": "/static/media/faq.1e9f0116.jpeg"
+  },
+  {
+    "revision": "fcaaf268c0ca70b3b0476b4c623bac51",
+    "url": "/static/media/farmer.fcaaf268.svg"
+  },
+  {
+    "revision": "9b6015b850197f85e4bdf4d460e9bcc3",
+    "url": "/static/media/home.9b6015b8.jpeg"
+  },
+  {
+    "revision": "7bc8f9392bae1a45861a49ab20d52589",
+    "url": "/static/media/logo.7bc8f939.png"
+  },
+  {
+    "revision": "88a8857f49d6e236df747dc8685c4f33",
+    "url": "/static/media/roadmap.88a8857f.jpeg"
+  },
+  {
+    "revision": "fe7b8c6747df594cb3777399ad9290c6",
+    "url": "/static/media/screen1.fe7b8c67.jpeg"
+  },
+  {
+    "revision": "cfb74ce5e59bf8b5e0aacdac651b6406",
+    "url": "/static/media/screen2.cfb74ce5.jpeg"
+  },
+  {
+    "revision": "9ab79f718126d184f708a858238c88ce",
+    "url": "/static/media/screen3.9ab79f71.jpeg"
+  },
+  {
+    "revision": "b9de82165915666dbb2816caa39dfe14",
+    "url": "/static/media/screen4.b9de8216.jpeg"
+  },
+  {
+    "revision": "c9beb5e407592ed97afaf28bf2e934e4",
+    "url": "/static/media/shopping-cart.c9beb5e4.svg"
+  },
+  {
+    "revision": "dea70ba441a4878d17c0c4de7b487231",
+    "url": "/static/media/user.dea70ba4.svg"
+  },
+  {
+    "revision": "6eedafee69c6406a2c64ea6d0e796164",
+    "url": "/static/media/vegetable.6eedafee.svg"
+  },
+  {
+    "revision": "31b7e9b62c231af4ad6610e75f6eee0e",
+    "url": "/static/media/wave.31b7e9b6.png"
+  },
+  {
+    "revision": "06bf648d094166952dcd30c255fc3fc2",
+    "url": "/static/media/white3.06bf648d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fuddi-Landing-Page/precache-manifest.3c23a80252da2a67f72aab317146ff60.js"
+  "/precache-manifest.c56e9c6cb833b3bb70495060ccb73886.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fuddi-Landing-Page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
