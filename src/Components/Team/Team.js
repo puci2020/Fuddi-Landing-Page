67902e8ef9de389b3a4a181d3eb2b860066f73bc
puci2020 <adam.pucicki97@gmail.com>
2020-09-29 14:17:58 +0200

    Update grid layout in Team component

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -5,7 +5,7 @@ import linkedin from '../../img/social_icons/linkedin2.png'
 import github from '../../img/social_icons/github.png'
 import {useTranslation} from "react-i18next";
 import face1 from '../../img/team/rafal_szuwalski.jpeg'
-import face2 from '../../img/team/2.jpg'
+import face2 from '../../img/team/krystian_szwedo.jpeg'
 import face3 from '../../img/team/szymon_mrozowski.jpeg'
 import face4 from '../../img/team/adam_pucicki.jpeg'
 import face5 from '../../img/team/5.jpg'
@@ -30,21 +30,97 @@ const H2 = styled.h2`
 const StyledGrid = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   grid-template-rows: repeat(2, 1fr);
   grid-gap: 100px;
   padding: 0 10%;
+  
+  .first{
+  grid-column: 1/3;
+  grid-row: 1;
+  }
+  
+  .second{
+  grid-column: 3/5;
+  grid-row: 1;
+  }
+  
+  .third{
+  grid-column: 5/7;
+  grid-row: 1;
+  }
+  
+  .four{
+    grid-column: 1/4;
+    grid-row: 2;
+  }
+  
+  .five{
+  grid-column: 4/7;
+  grid-row: 2;
+  }
+  
   ${({theme}) => theme.media.tablet}{
-      grid-template-columns: 1fr 1fr;
+      grid-template-columns: repeat(6, 1fr);
       grid-template-rows: repeat(3, 1fr);
       grid-gap: 10px;
+      
+      .first{
+  grid-column: 1/4;
+  grid-row: 1;
+  }
+  
+  .second{
+  grid-column: 4/7;
+  grid-row: 1;
+  }
+  
+  .third{
+  grid-column: 1/4;
+  grid-row: 2;
+  }
+  .four{
+    grid-column: 4/7;
+    grid-row: 2;
+  }
+  
+  .five{
+  grid-column: 1/7;
+  grid-row: 3;
+  }
+      
       }
    ${({theme}) => theme.media.phone}{
       grid-template-columns: 1fr;
-      grid-template-rows: repeat(6, 1fr);
+      grid-template-rows: repeat(5, 1fr);
       grid-gap: 10px;
+      
+          .first{
+  grid-column: 1/7;
+  grid-row: 1;
+  }
+  
+  .second{
+  grid-column: 1/7;
+  grid-row: 2;
+  }
+  
+  .third{
+  grid-column: 1/7;
+  grid-row: 3;
+  }
+  .four{
+    grid-column: 1/7;
+    grid-row: 4;
+  }
+  
+  .five{
+  grid-column: 1/7;
+  grid-row: 5;
+  }
       }
 `;
+
 
 const GridItem = styled.div`
   width: 100%;
@@ -54,6 +130,7 @@ const GridItem = styled.div`
   justify-content: space-between;
   flex-direction: column;
   padding: 20px;
+  
   
   .photo{
   width: 168px;
@@ -85,6 +162,8 @@ const GridItem = styled.div`
     text-align: center;
     }
   }
+  
+  
 `;
 const Social = styled.div`
   width: 100%;
@@ -106,7 +185,7 @@ const Team = () => {
         <StyledWrapper id="team">
             <H2>{t("team.header")}</H2>
             <StyledGrid>
-                <GridItem>
+                <GridItem className="first">
                     <div className="photo">
                         <img src={face1} alt="Radał Szuwalski"/>
                     </div>
@@ -114,68 +193,81 @@ const Team = () => {
                         <h4>Rafał Szuwalski</h4>
                         <h5>CEO & Co-founder</h5>
                         <Social>
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rafal-szuwalski/"> <img src={linkedin} alt="Linkedin icon"/></a>
+                            <a target="_blank" rel="noopener noreferrer"
+                               href="https://www.linkedin.com/in/rafal-szuwalski/"> <img src={linkedin}
+                                                                                         alt="Linkedin icon"/></a>
                         </Social>
                     </div>
                 </GridItem>
-                <GridItem>
-                    <div className="photo"><img src={face2} alt="Face1"/></div>
+                <GridItem className="second">
+                    <div className="photo"><img src={face2} alt="Krystian Szwedo"/></div>
                     <div className="desc">
                         <h4>Krystian Szwedo</h4>
                         <h5>CTO & Co-founder</h5>
                         <Social>
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/krystian-szwedo"> <img src={linkedin} alt="Linkedin icon"/></a>
-                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/S7f3dz14g1"><img src={github} alt="Github icon"/></a>
+                            <a target="_blank" rel="noopener noreferrer"
+                               href="https://www.linkedin.com/in/krystian-szwedo"> <img src={linkedin}
+                                                                                        alt="Linkedin icon"/></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/S7f3dz14g1"><img
+                                src={github} alt="Github icon"/></a>
                         </Social>
                     </div>
                 </GridItem>
-                <GridItem>
+                <GridItem className="third">
                     <div className="photo"><img src={face3} alt="Szymon Mrozowski"/></div>
                     <div className="desc">
                         <h4>Szymon Mrozowski</h4>
                         <h5>COO & Co-founder</h5>
                         <Social>
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szymon-mrozowski"> <img src={linkedin} alt="Linkedin icon"/></a>
-                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/mrozowski"><img src={github} alt="Github icon"/></a>
+                            <a target="_blank" rel="noopener noreferrer"
+                               href="https://www.linkedin.com/in/szymon-mrozowski"> <img src={linkedin}
+                                                                                         alt="Linkedin icon"/></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/mrozowski"><img
+                                src={github} alt="Github icon"/></a>
                         </Social>
                     </div>
                 </GridItem>
-                <GridItem>
-                    <div className="photo"><img src={face4} alt="Adam Pucicki"/></div>
-                    <div className="desc">
-                        <h4>Adam Pucicki</h4>
-                        <h5>Frontend Specialist<br/>
-                        & Co-founder</h5>
-                        <Social>
-                            <a target="_blank" rel="noopener noreferrer" href="http://linkedin.com/in/adampucicki"> <img src={linkedin} alt="Linkedin icon"/></a>
-                            <a target="_blank" rel="noopener noreferrer" href="http://github.com/puci2020"><img src={github} alt="Github icon"/></a>
-                        </Social>
-                    </div>
-                </GridItem>
-                <GridItem>
-                    <div className="photo"><img src={face6} alt="Face1"/></div>
-                    <div className="desc">
-                        <h4>John Doe</h4>
-                        <h5>Frontend Developer</h5>
-                        <Social>
-                            <img src={facebook} alt="Facebook icon"/>
-                            <img src={linkedin} alt="Linkedin icon"/>
-                            <img src={github} alt="Github icon"/>
-                        </Social>
-                    </div>
-                </GridItem>
-                <GridItem>
-                    <div className="photo"><img src={face5} alt="Face1"/></div>
-                    <div className="desc">
-                        <h4>Paulina Talarowska</h4>
-                        <h5>KAM</h5>
-                        <Social>
-                            <img src={facebook} alt="Facebook icon"/>
-                            <img src={linkedin} alt="Linkedin icon"/>
-                            <img src={github} alt="Github icon"/>
-                        </Social>
-                    </div>
-                </GridItem>
+
+                {/*<GridRow>*/}
+                    <GridItem className="four">
+                        <div className="photo"><img src={face4} alt="Adam Pucicki"/></div>
+                        <div className="desc">
+                            <h4>Adam Pucicki</h4>
+                            <h5>Frontend Specialist<br/>
+                                & Co-founder</h5>
+                            <Social>
+                                <a target="_blank" rel="noopener noreferrer" href="http://linkedin.com/in/adampucicki">
+                                    <img src={linkedin} alt="Linkedin icon"/></a>
+                                <a target="_blank" rel="noopener noreferrer" href="http://github.com/puci2020"><img
+                                    src={github} alt="Github icon"/></a>
+                            </Social>
+                        </div>
+                    </GridItem>
+                    {/*<GridItem>*/}
+                    {/*    <div className="photo"><img src={face6} alt="Face1"/></div>*/}
+                    {/*    <div className="desc">*/}
+                    {/*        <h4>John Doe</h4>*/}
+                    {/*        <h5>Frontend Developer</h5>*/}
+                    {/*        <Social>*/}
+                    {/*            <img src={facebook} alt="Facebook icon"/>*/}
+                    {/*            <img src={linkedin} alt="Linkedin icon"/>*/}
+                    {/*            <img src={github} alt="Github icon"/>*/}
+                    {/*        </Social>*/}
+                    {/*    </div>*/}
+                    {/*</GridItem>*/}
+                    <GridItem className="five">
+                        <div className="photo"><img src={face5} alt="Face1"/></div>
+                        <div className="desc">
+                            <h4>Paulina Talarowska</h4>
+                            <h5>KAM</h5>
+                            <Social>
+                                <img src={facebook} alt="Facebook icon"/>
+                                <img src={linkedin} alt="Linkedin icon"/>
+                                <img src={github} alt="Github icon"/>
+                            </Social>
+                        </div>
+                    </GridItem>
+                {/*</GridRow>*/}
             </StyledGrid>
         </StyledWrapper>
     );
