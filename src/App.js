958158bbb561631949a6da7c87cd4eec988384d8
puci2020 <adam.pucicki97@gmail.com>
2020-11-07 19:56:13 +0100

    Try to fix router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import Main from './Pages/Main'
 import Faq from "./Pages/Faq";
 import Contact from "./Pages/Contact";
@@ -10,15 +10,14 @@ import Shop from "./Pages/Shop/Shop";
 
 const App = () => {
     return (
-            <Switch basename={process.env.PUBLIC_URL}>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Route exact path={"/"} component={Main}/>
                 <Route path={"/faq"} component={Faq}/>
                 <Route path={"/contact"} component={Contact}/>
                 <Route path={"/newsletter"} component={Newsletter}/>
                 <Route path={"/crowdfunding"} component={Crowdfunding}/>
                 <Route path={"/shop"} component={Shop}/>
-                <Route component={Main}/>
-            </Switch>
+            </Router>
     );
 }
 
